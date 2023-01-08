8b27fd83303c7703906ee493959e41cec3acddff
Mert Yildiz <mert.yildiz@heyjobs.de>
2023-01-08 17:59:58 +0300

    feat: create vpc and subnet.

--- main.tf
+++ main.tf
@@ -1,1 +1,22 @@
 provider "aws" {}
+
+variable "environment" {}
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "availability_zone" {}
+
+resource "aws_vpc" "app-vpc" {
+  cidr_block = var.vpc_cidr_block
+  tags = {
+    Name = "${var.environment}-app-vpc"
+  }
+}
+
+resource "aws_subnet" "app-subnet" {
+  vpc_id = aws_vpc.app-vpc.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.availability_zone
+  tags = {
+    Name = "${var.environment}-app-subnet"
+  }
+}
