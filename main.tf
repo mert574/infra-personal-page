ccaba3c0e8eeb56b84572108aba9dd0529a76f36
Mert Yildiz <mert.yildiz@heyjobs.de>
2023-01-08 18:50:23 +0300

    feat: setup default security group

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ variable "environment" {}
 variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "availability_zone" {}
+variable "my_ip" {}
 
 resource "aws_vpc" "app-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -14,8 +15,8 @@ resource "aws_vpc" "app-vpc" {
 }
 
 resource "aws_subnet" "app-subnet" {
-  vpc_id = aws_vpc.app-vpc.id
-  cidr_block = var.subnet_cidr_block
+  vpc_id            = aws_vpc.app-vpc.id
+  cidr_block        = var.subnet_cidr_block
   availability_zone = var.availability_zone
 
   tags = {
@@ -41,5 +42,38 @@ resource "aws_default_route_table" "app-rtb" {
 
   tags = {
     Name = "${var.environment}-app-rtb"
+  }
+}
+
+resource "aws_default_security_group" "app-sg" {
+  vpc_id = aws_vpc.app-vpc.id
+
+  ingress {
+    description = "Allow SSH from my_ip"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    description = "Allow connections to 8080"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description     = "Allow outbound connections"
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "${var.environment}-app-default-sg"
   }
 }
