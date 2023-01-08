2edb8876b3b5d435e2280dd1c79a83b606e450a4
Mert Yildiz <mert.yildiz@heyjobs.de>
2023-01-08 18:19:28 +0300

    feat: connect vpc to internet

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ variable "availability_zone" {}
 
 resource "aws_vpc" "app-vpc" {
   cidr_block = var.vpc_cidr_block
+
   tags = {
     Name = "${var.environment}-app-vpc"
   }
@@ -16,7 +17,34 @@ resource "aws_subnet" "app-subnet" {
   vpc_id = aws_vpc.app-vpc.id
   cidr_block = var.subnet_cidr_block
   availability_zone = var.availability_zone
+
   tags = {
     Name = "${var.environment}-app-subnet"
   }
+}
+
+resource "aws_internet_gateway" "app-igw" {
+  vpc_id = aws_vpc.app-vpc.id
+
+  tags = {
+    Name = "${var.environment}-app-igw"
+  }
+}
+
+resource "aws_route_table" "app-rtb" {
+  vpc_id = aws_vpc.app-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.app-igw.id
+  }
+
+  tags = {
+    Name = "${var.environment}-app-rtb"
+  }
+}
+
+resource "aws_route_table_association" "app-rtb-assc" {
+  route_table_id = aws_route_table.app-rtb.id
+  subnet_id = aws_subnet.app-subnet.id
 }
